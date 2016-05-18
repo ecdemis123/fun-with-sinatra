30fd0240a5ab506c49d4a44ecb9b3ce669d1140e
Eleanor Demis <eleanordemis@gmail.com>
2016-05-18 13:13:49 -0700

    creates basic rb web server for static pages

---
+++ app.rb
@@ -0,0 +1,39 @@
+require('sinatra')
+require('sinatra/reloader')
+
+get('/') do
+  "<!DOCTYPE html>
+<html>
+<head>
+  <title>Hello Homie</title>
+  <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>
+</head>
+<body>
+  <h1>Hello Homie</h1>
+  <p>Dear Friend,</p>
+  <p>How are you? I hope that you are having a nice weekend. I'm vacationing in the Iceland while I learn programming! </p>
+  <p>Friend, you would not believe how cold it is here. I should have gone to Hawaii instead.</p>
+  <p>But I like programming a lot, so I've got that going for me. </p>
+  <p>Looking forward to seeing you soon. I'll bring you back a souvenir. </p>
+  <p>Cheers,</p>
+  <p>Travel Enthusiast Jane</p>
+
+</body>
+</html>"
+end
+
+get('/favorite_photos') do
+ "<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Hello Friend!</title>
+    <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>
+  </head>
+  <body>
+    <h1>Favorite Traveling Photos</h1>
+      <ul>
+        <li><img src='./public/img/canyon.jpg' alt='A photo of a canyon.'/></li>
+      </ul>
+  </body>
+  </html>"
+end

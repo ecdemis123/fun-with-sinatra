0cfceaf549337e237ebf5b112a0358c9695b5775
Eleanor Demis <eleanordemis@gmail.com>
2016-05-18 13:24:34 -0700

    removes layout html

--- app.rb
+++ app.rb
@@ -10,17 +10,5 @@ get('/australia') do
 end
 
 get('/favorite_photos') do
- "<!DOCTYPE html>
-  <html>
-  <head>
-    <title>Hello Friend!</title>
-    <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>
-  </head>
-  <body>
-    <h1>Favorite Traveling Photos</h1>
-      <ul>
-        <li><img src='./public/img/canyon.jpg' alt='A photo of a canyon.'/></li>
-      </ul>
-  </body>
-  </html>"
+  erb(:favorite_photos)
 end

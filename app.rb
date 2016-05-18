acf98e294b3179cc023f0459c7ef8952a4f411e6
Eleanor Demis <eleanordemis@gmail.com>
2016-05-18 15:21:17 -0700

    adds greeting card form

--- app.rb
+++ app.rb
@@ -2,13 +2,26 @@ require('sinatra')
 require('sinatra/reloader')
 
 get('/') do
+  @friend = "Jessica"
   erb(:letter)
 end
 
 get('/australia') do
+  @sender = "Jane"
+  @recipient = "Jessica"
   erb(:australia)
 end
 
 get('/favorite_photos') do
   erb(:favorite_photos)
+end
+
+get('/greeting_form') do
+  erb(:form)
+end
+
+get('/greeting_card') do
+  @sender = params.fetch('sender')
+  @recipient = params.fetch('recipient')
+  erb(:greeting_card)
 end
